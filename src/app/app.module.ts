2357f4e51fd256fef443a142ea61a352c746f578
borgepl <pdeborger@gmail.com>
2022-08-11 16:11:27 +0200

    AuthModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,40 +4,32 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from  '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { WarningAlertComponent } from './components/shared/warning-alert/warning-alert.component';
-import { InfoAlertComponent } from './components/shared/info-alert/info-alert.component';
 import { HeaderComponent } from './components/header/header.component';
-import { DropdownDirective } from './components/shared/dropdown.directive';
 import { ShoppingListService } from './components/shopping-list/shopping-list.service';
 import { RecipeService } from './components/recipes/recipe.service';
 import { AppRoutingModule } from './app-routing.module';
 import { DataStorageService } from './components/shared/data-storage.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './components/shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './components/shared/alert/alert.component';
-import { PlaceholderDirective } from './components/shared/placeholder/placeholder.directive';
 import { RecipesModule } from './components/recipes/recipes.module';
 import { ShoppingListModule } from './components/shopping-list/shopping-list.module';
 import { SharedModule } from './components/shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 
 
 @NgModule({
   declarations: [
     AppComponent, 
-    HeaderComponent,
-    AuthComponent
-   
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
-    SharedModule
+    SharedModule,
+    AuthModule
   ],
   providers: [ShoppingListService,RecipeService, DataStorageService,
               {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../components/shared/shared.module";
+import { AuthComponent } from "./auth.component";
+
+@NgModule({
+    declarations: [AuthComponent],
+    imports: [FormsModule, SharedModule],
+    exports: [AuthComponent]
+})
+export class AuthModule {}
